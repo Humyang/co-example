97b83f0d53f61244ed815291a8cddc4f21785d95
Yangxg <jessebutchman@gmail.com>
2016-11-23 18:00:14 +0800

    update

--- advance.js
+++ advance.js
@@ -3,9 +3,23 @@ function * ye1(){
     let a = yield Promise.resolve(1)
     return a
 }
+function promise1(pram){
+    return new Promise(function(resolve, reject){
+        if(pram){
+            resolve(true)
+        }else{
+            reject(false)
+        }
+    })
+}
 co(function *(){
     let a = yield ye1
     let b = yield ye1
-
+    let c = yield promise1(true)
     console.log(a,b)
+    console.log('c: ',c)
+    let d = yield promise1(false)
+
+}).catch(function(err){
+    console.log('err：',err)
 })
