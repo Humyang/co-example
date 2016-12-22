900ff03ba9dc16cea5c1ce9e90321903fe89e3c3
Yangxg <jessebutchman@gmail.com>
2016-12-22 10:53:36 +0800

    蓝色
    测试 promise 多次 catch 的方式

--- advance.js
+++ advance.js
@@ -18,15 +18,48 @@ function p_s(){
         return 'from promise\' then' 
     })
 }
-co(function *(){
-    let a = yield ye1
-    let b = yield ye1
-    let c = yield promise1(true)
-    console.log(a,b)
-    console.log('c: ',c)
-    // let d = yield promise1(false)
-    let d = yield p_s()
-    console.log('d: ',d)
+// co(function *(){
+//     let a = yield ye1
+//     let b = yield ye1
+//     let c = yield promise1(true)
+//     console.log(a,b)
+//     console.log('c: ',c)
+//     // let d = yield promise1(false)
+//     let d = yield p_s()
+//     console.log('d: ',d)
+
+
+
+// }).catch(function(err){
+//     console.log('err：',err)
+// })
+
+function promise2(pram){
+    return new Promise(function(resolve, reject){
+        if(pram){
+            resolve(true)
+        }else{
+            reject(false)
+        }
+    }).then(function(res){
+        console.log('recive from promise: ,',res)
+        return 'from promise\' then' 
+    })
+}
+function p_s2(){
+    return promise2(false).then(function(res){
+        console.log('recive from promise: ,',res)
+        return 'from promise\' then' 
+    }).catch(function(err){
+        console.log('error catched:',err)
+        throw new Error(err)
+    })
+}
+co(function*(){
+    let e = yield p_s2()
+
+    console.log("from promise 2")
+    console.log(e)
 }).catch(function(err){
-    console.log('err：',err)
+    console.log('error catched2:',err)
 })
